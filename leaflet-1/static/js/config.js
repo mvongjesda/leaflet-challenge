30c273062c7f91e22546984dfe6a0b47d8428462
mvongjesda <marsha.vongjesda@gmail.com>
2020-04-06 15:54:17 -0400

    Update logic.js

--- leaflet-1/static/js/config.js
+++
@@ -1,2 +0,0 @@
-// API key
-const API_KEY = "enter API key here";
--- leaflet-1/static/js/logic.js
+++ leaflet-1/static/js/logic.js
@@ -1,16 +1,72 @@
-let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+// Connect to geojson USGS information using D3
 
-d3.json(queryUrl, function(data) {
+let usgsUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+
+d3.json(usgsUrl, function(data) {
 
   createFeatures(data.features);
 });
 
-function createMap(earthquakes) {
+function createFeatures(earthquakeInfo) {
 
-  // Define map layer
-  let map = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 10,
-    id: "map",
-    accessToken: API_KEY// Store our API endpoint inside queryUrl
-    let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+
+    let earthquakes = L.geoJSON(earthquakeInfo, {
+      onEachFeature: onEachFeature,
+      pointToLayer: function (feature, latlng) {
+        let color;
+        let r = 250;
+        let g = Math.floor(r-50*feature.properties.mag);
+        let b = Math.floor(r-50*feature.properties.mag);
+        color= "rgb("+r+" ,"+g+","+ b+")"
+        
+        let markerOptions = {
+          
+          fillColor: color,
+          color: "white",
+          weight: 1.5,
+          fillOpacity: 0.75,
+          opacity: 0.8,
+          radius: 5*feature.properties.mag
+          
+        };
+        return L.circleMarker(latlng, markerOptions);
+      }
+    });
+  // Create pop-up with the magnitude and location
+  function onEachFeature(feature, layer) {
+    layer.bindPopup("<h3>" + "Magnitude: " + feature.properties.mag + "</h3><p>" + feature.properties.place + "</p>");
+  }
+    createMap(earthquakes);
+  }
+  
+
+  function createMap(earthquakes) {
+
+    let lightmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+      maxZoom: 10,
+      id: "mapbox.light",
+      accessToken: "pk.eyJ1IjoibWFyc2hhdiIsImEiOiJjazd6Y3I3cXQwNDh1M2tybGs3eDEwbzRuIn0.qvF35_fYHd8ZTq3dpth3sQ"
+    });
+  
+ 
+    // Base layer
+    let baseLayer = {
+      "Land Map": lightmap
+    };
+  
+    // Overlay layer
+    let overlayLayer = {
+      Earthquakes: earthquakes
+    };
+  
+    // Create our map, with the coordinates of the geographical center of the US
+    let myMap = L.map("map", {
+      center: [39.50, -98.35],
+      zoom: 4,
+      layers: [lightmap, earthquakes]
+    });
+
+    L.control.layers(baseLayer, overlayLayer).addTo(myMap);
+
+}
